e56510f4e7fc80314d3c6f1cdfdf0cc49c82b42b
Unknown <arkhomk@yahoo.com>
2019-01-26 21:27:15 +0700

    Display Time only

--- src/main/webapp/app/entities/service-provider/service-provider-update.component.ts
+++ src/main/webapp/app/entities/service-provider/service-provider-update.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import * as moment from 'moment';
-import { DATE_TIME_FORMAT } from 'app/shared/constants/input.constants';
+import { DATE_TIME_FORMAT, TIME_ONLY } from 'app/shared/constants/input.constants';
 
 import { IServiceProvider } from 'app/shared/model/service-provider.model';
 import { ServiceProviderService } from './service-provider.service';
@@ -24,9 +24,8 @@ export class ServiceProviderUpdateComponent implements OnInit {
         this.isSaving = false;
         this.activatedRoute.data.subscribe(({ serviceProvider }) => {
             this.serviceProvider = serviceProvider;
-            this.serviceStart =
-                this.serviceProvider.serviceStart != null ? this.serviceProvider.serviceStart.format(DATE_TIME_FORMAT) : null;
-            this.serviceEnd = this.serviceProvider.serviceEnd != null ? this.serviceProvider.serviceEnd.format(DATE_TIME_FORMAT) : null;
+            this.serviceStart = this.serviceProvider.serviceStart != null ? this.serviceProvider.serviceStart.format(TIME_ONLY) : null;
+            this.serviceEnd = this.serviceProvider.serviceEnd != null ? this.serviceProvider.serviceEnd.format(TIME_ONLY) : null;
         });
     }
 
@@ -36,8 +35,8 @@ export class ServiceProviderUpdateComponent implements OnInit {
 
     save() {
         this.isSaving = true;
-        this.serviceProvider.serviceStart = this.serviceStart != null ? moment(this.serviceStart, DATE_TIME_FORMAT) : null;
-        this.serviceProvider.serviceEnd = this.serviceEnd != null ? moment(this.serviceEnd, DATE_TIME_FORMAT) : null;
+        this.serviceProvider.serviceStart = this.serviceStart != null ? moment(this.serviceStart, TIME_ONLY) : null;
+        this.serviceProvider.serviceEnd = this.serviceEnd != null ? moment(this.serviceEnd, TIME_ONLY) : null;
         if (this.serviceProvider.id !== undefined) {
             this.subscribeToSaveResponse(this.serviceProviderService.update(this.serviceProvider));
         } else {
